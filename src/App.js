332b934068a815355461bda04181aaad9de3a863
William José Sánchez Gutiérrez <willjsg@hotmail.com>
2022-05-22 10:04:30 -0600

    - Se actualiza a la nueva versi[on de node.js - Se ajusta configuración en package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
+function App(){
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        setLoading(true)
+        axios({
+            method: 'GET',
+            baseURL: 'https://fakestoreapi.com',
+            url: '/products',
+          })
+            .then(({ data }) => {
+              setData(data)
+            })
+            .catch(err => console.dir(err))
+            .finally(() => setLoading(false))
+    }, [])
+
+    return (  
+      <section className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt='logo'/>
+        <h1>Fake Shop</h1>
+        </header>
+        {loading && "Loading..."}
+        {!!data && data.length > 0 ? data.map((product) => {
+            return(
+              <article key={product.id}>
+                <h2>title: {product.title}</h2>
+                <p>id: {product.id}</p>
+                <p>description: {product.description}</p>
+                <p>price: {product.price}</p>
+                <p>category: {product.category}</p>
+                <img src={product.image} className="thumbnail" alt={product.description}/>
+              </article>
+            )   
+          }):(<p>API did not provided any product, try again.</p>)
+        }
+      </section>
+    )
+}
 export default App;
